b75d5ae461f06d16b8510dea2d54d1ba0d7fe25e
bhargav1000 <bhargavsagiraju@gmail.com>
2020-08-28 00:17:25 +0530

    added central limit theorem

--- basics.R
+++ basics.R
@@ -58,4 +58,103 @@ obs = mean(x)
 
 mean(abs(nulls-obs)>1)
   
-  
+########################################################
+library(gapminder)
+data(gapminder)
+head(gapminder)
+x=gapminder %>% 
+  filter(year==1952) %>% 
+  select(country, lifeExp) %>% unlist() %>% 
+  as.vector("numeric")
+
+gapminder %>% 
+  filter(year==1952) %>% 
+  summarise(mean(lifeExp>=40 & lifeExp<=60))
+
+prop = function(q) {
+  mean(x <= q)
+}
+
+prop(40)
+
+qs = seq(from=min(x), to=max(x), length=20)
+
+props = sapply(qs, prop)
+
+plot(qs, props)
+props = sapply(qs, function(q) mean(x <= q))
+plot(ecdf(x))
+################################################
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+x <- unlist( read.csv(filename) )
+
+# make averages5
+set.seed(1)
+n <- 1000
+averages5 <- vector("numeric",n)
+for(i in 1:n){
+  X <- sample(x,5)
+  averages5[i] <- mean(X)
+}
+
+# make averages50
+set.seed(1)
+n <- 1000
+averages50 <- vector("numeric",n)
+for(i in 1:n){
+  X <- sample(x,50)
+  averages50[i] <- mean(X)
+}
+
+
+par(mfrow=c(1,2))
+hist(averages5)
+hist(averages50)
+
+pnorm(25,23.9, 0.43) - pnorm(23,23.9, 0.43)
+################################################
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/mice_pheno.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+dat <- read.csv(filename) 
+
+dat <- na.omit( dat )
+
+library(dplyr)
+x <- dat %>%
+  filter(Sex=='F') %>% 
+  filter(Diet=='chow') %>% 
+  select(Bodyweight) %>% 
+  unlist()
+
+mean(x)
+
+library(rafalib)
+popsd(x)
+
+set.seed(2)
+X=sample(x, 25)
+
+mean(X)
+
+y=dat %>% 
+  filter(Sex=='F') %>% 
+  filter(Diet=='hf') %>% 
+  select(Bodyweight) %>% 
+  unlist()
+
+mean(y)
+popsd(y)
+
+set.seed(2)
+Y=sample(y, 25)
+
+mean(Y)
+
+diff1=mean(y)-mean(x)
+diff2=mean(Y)-mean(X)
+abs(diff1-diff2)
